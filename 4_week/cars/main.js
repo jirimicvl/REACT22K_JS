6318f2ce77c1164ebfde487644277618afdf295d
Margit Tennosaar <46651865+margittennosaar@users.noreply.github.com>
2022-02-10 14:37:39 +0200

    makeTable function which is making a table based on array

--- 4_week/cars/main.js
+++ 4_week/cars/main.js
@@ -1,4 +1,5 @@
 let formInput = document.querySelector("#inputData");
+let resultTable = document.querySelector(".result");
 
 class Car {
   constructor(licence, maker, model, owner, price, color) {
@@ -10,7 +11,9 @@ class Car {
     this.color = color;
   }
 }
-const carArray = [];
+const carArray = [
+  { licence: "", maker: "", model: "", owner: "", price: "", color: "" },
+];
 
 const addCar = (event) => {
   event.preventDefault();
@@ -24,10 +27,30 @@ const addCar = (event) => {
   let newCar = new Car(licence, maker, model, owner, price, color);
 
   carArray.push(newCar);
-
   console.table(carArray);
-
   console.log("form was submitted", licence, maker, model, owner, price, color);
+  makeTable();
 };
+
+const makeTable = () => {
+  let result = "<table border=1 >";
+  result += "<tr>";
+  for (const key in carArray[0]) {
+    result += "<th>" + key + "</th>";
+  }
+  result += "</tr>";
+  for (let i = 1; i < carArray.length; i++) {
+    result += "<tr>";
+    for (const key in carArray[i]) {
+      result += "<td>" + carArray[i][key] + "</td>";
+    }
+    result += "</tr>";
+  }
+  result += "</table>";
+  resultTable.innerHTML = result;
+  console.log(result);
+};
+
+makeTable();
 
 formInput.addEventListener("submit", addCar);
